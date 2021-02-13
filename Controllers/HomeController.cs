ba1009b3a7343086f9f0de4f3bc47b70683e7479
Patty <pattyguiselle@hotmail.com>
2021-02-12 21:59:23 -0600

    index on rama-patty: 2314711 Vistas Alum y docentes listas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tutorias.Repositories;
+using Tutorias.Models;
 
 namespace Tutorias.Controllers
 {
@@ -13,15 +14,71 @@ namespace Tutorias.Controllers
         {
             return View();
         }
+
         [Route("RegistroAlumnos")]
         public IActionResult RegistroAlumnos()
         {
             return View();
         }
+
+  
+        [HttpPost]
+        [Route("RegistroAlumnos")]
+        public IActionResult RegistroAlumnos(AlumnoViewModel alumno)
+        {
+            if (alumno != null)
+            {
+                string error = Validar(alumno);
+
+                if (error != "")
+                {
+                    ModelState.AddModelError("Error", error);
+                    return View(alumno);
+                }
+
+                try
+                {
+                    AlumnoRepository repos = new AlumnoRepository();
+                    repos.InsertAlumno(alumno);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("Error", ex.Message);
+                    return View(alumno);
+
+                }
+            }
+
+            return RedirectToAction("RegistroAlumnos");
+        }
+
         [Route("RegistroMaestros")]
         public IActionResult RegistroMaestros()
         {
             return View();
+        }
+
+        string Validar(AlumnoViewModel alumno)
+        {
+            if (string.IsNullOrEmpty(alumno.NumeroControl))
+                return "Proporcione su numero de control";
+            if (string.IsNullOrEmpty(alumno.Nombre))
+                return "Proporcione su nombre";
+            if (string.IsNullOrEmpty(alumno.ApPaterno))
+                return "Proporcione su apellido paterno";
+            if (string.IsNullOrEmpty(alumno.ApMaterno))
+                return "Proporcione su apellido materno";
+            if (string.IsNullOrEmpty(alumno.Email))
+                return "Proporcione su email";
+            if (string.IsNullOrEmpty(alumno.Contraseña))
+                return "Proporcione su contraseña";
+            if (alumno.IdSemestre == 0)
+                return "Proporcione su semestre";
+            if (alumno.IdCarrera == 0)
+                return "Proporcione su carrera";
+
+            return "";
+
         }
     }
 }
--- Models/ViewModels/AlumnoViewModel.cs
+++ Models/ViewModels/AlumnoViewModel.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Tutorias.Models
 {
-    public partial class Alumno
+    public class AlumnoViewModel
     {
-        public Alumno()
-        {
-            Alumnosmateria = new HashSet<Alumnosmateria>();
-        }
-
         public string NumeroControl { get; set; }
         public string Nombres { get; set; }
         public string ApPaterno { get; set; }
@@ -18,11 +15,6 @@ namespace Tutorias.Models
         public string Contraseña { get; set; }
         public int IdSemestre { get; set; }
         public int IdCarrera { get; set; }
-        public int IdRol { get; set; }
-
-        public Carrera IdCarreraNavigation { get; set; }
-        public Roles IdRolNavigation { get; set; }
-        public Semestre IdSemestreNavigation { get; set; }
-        public ICollection<Alumnosmateria> Alumnosmateria { get; set; }
+        //public int IdRol { get; set; }
     }
 }

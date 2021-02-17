5ca5aee36bc5c9fdef5cf90cb9c6326dd39ae976
RomarioR98 <romario_rodriguez@outlook.es>
2021-02-16 22:53:51 -0600

    Agregar maestro listo

---
+++ Models/ViewModels/MaestroViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorias.Models
+{
+    public class MaestroViewModel
+    {
+        public string NumeroControl { get; set; }
+        public string Nombres { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public int IdRol { get; set; }
+    }
+}
--- Repositories/MaestroRepository.cs
+++ Repositories/MaestroRepository.cs
@@ -9,6 +9,75 @@ namespace Tutorias.Repositories
 {
     public class MaestroRepository : Repository<Maestro>
     {
+        public void InsertMaestro(MaestroViewModel maestro)
+        {
 
+            string error = Validar(maestro);
+
+            if (error != "")
+                throw new ArgumentException(error);
+
+            if (GetAll().Any(x => x.NumeroControl == maestro.NumeroControl))
+                throw new ArgumentException("El numero de control ya ha sido registrado");
+            if (GetAll().Any(x => x.Email == maestro.Email))
+                throw new ArgumentException("El email ya ha sido registrado");
+
+            Maestro ma = new Maestro()
+            {
+                NumeroControl = maestro.NumeroControl.ToLower(),
+                Nombres = maestro.Nombres,
+                ApPaterno = maestro.ApPaterno,
+                ApMaterno = maestro.ApMaterno,
+                Email = maestro.Email,
+                Contraseña = Encrypt.GetMD5(maestro.Contraseña),
+                IdRol = 2
+            };
+            Insert(ma);
+        }
+
+        public void UpdateMaestro(MaestroViewModel maestro)
+        {
+            if (GetById(maestro.NumeroControl).NumeroControl == maestro.NumeroControl.ToLower())
+                return;
+
+            string error = Validar(maestro);
+
+            if (error != "")
+                throw new ApplicationException(error);
+
+            if (GetAll().Any(x => x.Email == maestro.Email))
+                throw new ArgumentException("El email ya ha sido registrado");
+
+            var maestroBD = GetById(maestro.NumeroControl);
+
+            maestroBD.Nombres = maestro.Nombres;
+            maestroBD.ApPaterno = maestro.ApPaterno;
+            maestroBD.ApMaterno = maestro.ApMaterno;
+            maestroBD.Email = maestro.Email;
+            maestroBD.Contraseña = Encrypt.GetMD5(maestro.Contraseña);
+            Update(maestroBD);
+
+
+        }
+
+
+        string Validar(MaestroViewModel maestro)
+        {
+            if (string.IsNullOrEmpty(maestro.NumeroControl))
+                return "Proporcione su numero de control";
+            if (string.IsNullOrEmpty(maestro.Nombres))
+                return "Proporcione su nombre";
+            if (string.IsNullOrEmpty(maestro.ApPaterno))
+                return "Proporcione su apellido paterno";
+            if (string.IsNullOrEmpty(maestro.ApMaterno))
+                return "Proporcione su apellido materno";
+            if (string.IsNullOrEmpty(maestro.Email))
+                return "Proporcione su email";
+            if (string.IsNullOrEmpty(maestro.Contraseña))
+                return "Proporcione su contraseña";
+
+            return "";
+
+        }
     }
 }
